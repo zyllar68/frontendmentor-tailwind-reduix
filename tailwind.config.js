19990a501b004cbd8887e87d7d225508c4a64860
zyllar68 <zyrilmancion@gmail.com>
2022-07-18 01:24:56 +0800

    added eslint-parser and babel/preset-react and configure tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     colors: {
       'dark-bullace': '#3a2244',
       'semi-dark-bullace': '#614e69',
-      'bullace': '#897a8f',
+      bullace: '#897a8f',
       'light-bullace': '#b0a7b4',
-      'font': '#d8d3da',
+      font: '#d8d3da',
+      black: '#000000',
+      white: '#ffffff',
     },
     padding: {
       DEFAULT: '1rem',
