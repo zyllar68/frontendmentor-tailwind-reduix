5533d7ebd87a5b91f3aae69fceeb0499e1950874
zyllar68 <zyrilmancion@gmail.com>
2022-07-18 01:28:55 +0800

    edit header component to fit the button in screen

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,12 @@
 const Header = () => {
   return (
-    <div className="flex flex-row items-center justify-between  p h-24 bg-semi-dark-bullace">
-      <h1 className="text-2xl font-medium text-font">Platform Launch</h1>
-      <button type="button" className="p-xsm w-40 text-font rounded-full text-center float-right bg-dark-bullace hover:bg-bullace" onClick={() => { return alert('yeah'); }}>
-        Add New Task
-      </button>
+    <div className="bg-semi-dark-bullace">
+      <div className="pl-sm pr-sm flex items-center h-24 justify-between">
+        <h1 className="text-2xl font-medium text-font">Platform Launch</h1>
+        <button type="button" className="p-xsm w-40 text-font rounded-full bg-dark-bullace hover:bg-bullace" onClick={() => { return alert('yeah'); }}>
+          Add New Task
+        </button>
+      </div>
     </div>
   );
 };
