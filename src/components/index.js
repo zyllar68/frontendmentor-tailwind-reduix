cdd7633d1b9e14787a4b63f4dffd3e3c4f4e50ec
zyllar68 <zyrilmancion@gmail.com>
2022-07-18 01:30:42 +0800

    added all new components in export file

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export { default as Sidebar } from './Sidebar';
 export { default as Header } from './Header';
-export { default as TaskCard } from './TaskCard';
+export { default as TaskBoard } from './TaskBoard';
+export { default as AddTask } from './AddTask';
+export { default as Input } from './Input';
+export { default as Button } from './Button';
